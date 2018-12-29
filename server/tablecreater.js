1c077ca7fe386f41d801f4c26a582658eb67dc4a
Kenneth <kenny.laprelle@gmail.com>
2018-12-28 16:03:46 -0800

    internet explorer

--- server/tablecreater.js
+++ server/tablecreater.js
@@ -1,13 +1,14 @@
 const blogs = [
-  'FHFA Announces Conforming Loan Limit Increase In 2019',
-  'Nervous About Buying? Here’s A Dose of Confidence',
-  'Can I Still Get A VA Loan With Bad Credit?',
-  'Benchmark and The Patriot Tour by Team Never Quit',
 
-  'How To Avoid Roadblocks Before Closing On Your New Home',
+     'FHFA Announces Conforming Loan Limit Increase In 2019',
+    'Nervous About Buying? Here’s A Dose of Confidence',
+     'Can I Still Get A VA Loan With Bad Credit?',
+    'Benchmark and The Patriot Tour by Team Never Quit',
 
-  'Construction Loans: What to Expect',
+    'How To Avoid Roadblocks Before Closing On Your New Home',
 
-  'Benchmark Introduces New Program to Expand Options for Medical Professionals',
+    'Construction Loans: What to Expect',
+
+    'Benchmark Introduces New Program to Expand Options for Medical Professionals',
  
-];
+]
