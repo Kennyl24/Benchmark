ec276a09c2983cebfa4c7b6ed4181a39f4406476
Kenneth <kenny.laprelle@gmail.com>
2018-12-29 08:19:07 -0800

    internet explorer

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,7 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: [
-    'babel-polyfill', // <----- Add this
-    `${SRC_DIR}/index.jsx`,
-  ],
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
@@ -18,7 +15,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,      
         query: {
-          presets: [ 'babel-polyfill', 'react', 'react-dom', 'es2015', 'stage-0']
+          presets: [ 'react', 'es2015', 'stage-0']
         }
       }, {
         test: /\.scss$/,
