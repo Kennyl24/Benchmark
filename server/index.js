d0828c360b27490421ebb3e5b8b22fdf13e93076
Kenneth <kenny.laprelle@gmail.com>
2019-07-12 09:06:56 -0700

    sitemap

--- server/index.js
+++ server/index.js
@@ -51,6 +51,7 @@ app.use('/', express.static(__dirname + '/../client/dist'));
 app.use('/Home', express.static(__dirname + '/../client/dist'));
 app.use('/About', express.static(__dirname + '/../client/dist'));
 app.use('/Contact', express.static(__dirname + '/../client/dist'));
+app.use('/Calculator', express.static(__dirname + '/../client/dist'));
 app.use('/Blog', express.static(__dirname + '/../client/dist'));
 app.use('/Vlog', express.static(__dirname + '/../client/dist'));
 app.use('/AboutMe', express.static(__dirname + '/../client/dist'));
