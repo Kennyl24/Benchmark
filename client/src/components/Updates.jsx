51b8324cfccbfb62fbd854aabdcf74194147222c
Kenneth <kenny.laprelle@gmail.com>
2019-09-13 17:00:17 -0700

    update fix

--- client/src/components/Updates.jsx
+++ client/src/components/Updates.jsx
@@ -6,6 +6,7 @@ import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router-dom';
 import LeadingBar from './LeadingBar.jsx';
 import BottomNav from './BottomNav.jsx';
+import {Helmet} from "react-helmet";
 
 class Updates extends React.Component { 
   constructor(props) {
