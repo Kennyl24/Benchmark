eb282b6426f2f0385a6f69f094d7926661ddcdcf
Kenneth <kenny.laprelle@gmail.com>
2019-09-20 13:22:17 -0700

    new

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
     path: DIST_DIR
   },
   plugins: [
-    new MinifyPlugin()
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin()//Merge chunks 
   ],
   module: {
     rules: [
