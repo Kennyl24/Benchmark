11aa4f24d1d74acad3d1f774c359b8acdd967381
Kenneth <kenny.laprelle@gmail.com>
2019-11-22 11:41:57 -0800

    new updates

--- client/src/components/Updates.jsx
+++ client/src/components/Updates.jsx
@@ -39,7 +39,7 @@ class Updates extends React.Component {
    width="100%"
    style={{marginTop:'130px'}}
    height="1500px"
-   src='http://vip.vantageproduction2.com/HtmlTemplate/5062433/7c6db342-673c-40ba-bdf3-8b74cf94ac9d/Render?preview'>  
+   src='https://vip.vantageproduction2.com/HtmlTemplate/5064192/7c6db342-673c-40ba-bdf3-8b74cf94ac9d/Render?ver=3&partnerid=0'>  
      </iframe>
      <BottomNav/>
    </MuiThemeProvider>
